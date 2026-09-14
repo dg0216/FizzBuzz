e1fad204095af57c58d014daaa8aa21cb9133963
dg0216 <donggyuoh2007@gmail.com>
2026-09-14 13:59:31 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,7 +7,9 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
+        int i = 0;
+
+        while (i < 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +33,8 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+
+            i += 1;
         }
     }
 }
